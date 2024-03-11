fb479c70d44e95bd5660c884061ba72aa2a04b62
Psami-wondah <okechukwusamuel16@gmail.com>
2024-03-11 10:07:18 +0100

    feat: Setup flow for duplication of assets on user signup

--- src/app/modules/callback-module/index.tsx
+++ src/app/modules/callback-module/index.tsx
@@ -3,13 +3,36 @@ import Box from "@material-ui/core/Box";
 import { useHistory } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { PageLoader } from "app/modules/common/page-loader";
+import axios from "axios";
+import { useStoreState } from "app/state/store/hooks";
 
 function AuthCallbackModule() {
   const history = useHistory();
   const { error, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [loading, setLoading] = React.useState(true);
+
+  const duplicateAssets = async () => {
+    getAccessTokenSilently().then(async (newToken) => {
+      await axios.post(
+        `${process.env.REACT_APP_API}/users/duplicate-assets`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${newToken}`,
+          },
+        }
+      );
+    });
+  };
 
   React.useEffect(() => {
     if (isAuthenticated) {
+      (async () => {
+        setLoading(true);
+        await duplicateAssets();
+        setLoading(false);
+      })();
       if (localStorage.getItem("signup-state") == "true") {
         history.replace("/report/new/initial");
         localStorage.removeItem("signup-state");
@@ -23,7 +46,7 @@ function AuthCallbackModule() {
 
   return (
     <Box>
-      {!error && <PageLoader />}
+      {(!error || loading) && <PageLoader />}
       {error && <Box>{error.message}</Box>}
     </Box>
   );
