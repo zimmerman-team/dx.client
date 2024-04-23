1d2a71b3b1c2d77aaa1d4535d74d03ebb7f8c817
Psami-wondah <okechukwusamuel16@gmail.com>
2024-04-23 18:42:33 +0100

    feat: Rephrase the DELETE messaging DX-1112

--- src/app/components/Dialogs/deleteDatasetDialog.tsx
+++ src/app/components/Dialogs/deleteDatasetDialog.tsx
@@ -1,7 +1,8 @@
-import { IconButton, Modal } from "@material-ui/core";
+import { IconButton, Modal, createStyles, makeStyles } from "@material-ui/core";
 import { CloseOutlined } from "@material-ui/icons";
+import useGetChartsReportsCountByDataset from "app/hooks/useGetChartsReportsCountByDataset";
+import CircleLoader from "app/modules/home-module/components/Loader";
 import React from "react";
-import { useStyles } from "./deleteChartDialog";
 
 interface Props {
   modalDisplay: boolean;
@@ -11,6 +12,28 @@ interface Props {
   enableButton: boolean;
   setEnableButton: (value: React.SetStateAction<boolean>) => void;
 }
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: "100%",
+      height: "100%",
+    },
+    paper: {
+      outline: 0,
+      width: 646,
+      borderRadius: "10px",
+      position: "absolute",
+      padding: "2.5rem 2.5rem 2.5rem 4.5rem",
+      backgroundColor: "#fff",
+      boxShadow:
+        "0px 14.8787px 22.318px rgba(0, 0, 0, 0.05), 0px 4.4636px 7.43933px rgba(0, 0, 0, 0.05), 0px 0.743933px 7.43933px rgba(0, 0, 0, 0.05)",
+    },
+  })
+);
 
 export default function DeleteDatasetDialog(props: Props) {
   const classes = useStyles();
@@ -28,6 +51,10 @@ export default function DeleteDatasetDialog(props: Props) {
     }
   };
 
+  const { data, loading } = useGetChartsReportsCountByDataset(props.cardId);
+
+  console.log(data);
+
   return (
     <div>
       <Modal
@@ -37,107 +64,120 @@ export default function DeleteDatasetDialog(props: Props) {
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <div className={classes.paper}>
-          <form
-            onSubmit={() => props.handleDelete(props.cardId)}
-            data-cy="delete-dataset-item-form"
-          >
-            <div
-              css={`
-                width: 80%;
-                position: relative;
-              `}
+        {loading ? (
+          <div className="w-full h-[80px] flex items-center justify-center">
+            <CircleLoader />
+          </div>
+        ) : (
+          <div className={classes.paper}>
+            <form
+              onSubmit={() => props.handleDelete(props.cardId)}
+              data-cy="delete-dataset-item-form"
             >
-              <IconButton
-                onClick={() => props.setModalDisplay(false)}
-                css={`
-                  position: absolute;
-                  right: -93px;
-                  top: -16px;
-                  color: "#231D2C";
-                `}
-              >
-                <CloseOutlined color="inherit" />
-              </IconButton>
-              <p
-                css={`
-                  font-weight: 400;
-                  font-size: 34px;
-                  color: #231d2c;
-                  line-height: 41px;
-                  margin-bottom: 0px;
-                `}
-              >
-                Delete dataset
-              </p>
-              <p
-                css={`
-                  margin-top: 3px;
-                `}
-              >
-                Absolutely sure you want to delete the dataset(s)? <br />{" "}
-                <b>This action is irreversible!</b>
-              </p>
+              <div>
+                <IconButton
+                  onClick={() => props.setModalDisplay(false)}
+                  css={`
+                    position: absolute;
+                    right: 20px;
+                    top: 18px;
+                    color: "#231D2C";
+                  `}
+                >
+                  <CloseOutlined color="inherit" />
+                </IconButton>
+                <p
+                  css={`
+                    font-weight: 400;
+                    font-size: 34px;
+                    padding: 0;
+                    margin: 0;
+                    color: #231d2c;
+                    line-height: 41px;
+                    margin-bottom: 0px;
+                  `}
+                >
+                  Delete dataset
+                </p>
+                <p
+                  css={`
+                    margin-top: 8px;
+                    font-size: 16px;
+                  `}
+                >
+                  By deleting this dataset you and other users will lose access
+                  to
+                  <br />
+                  this data and will affect usage of some charts and reports. A
+                  <br />
+                  total of {data.chartsCount} charts and {data.reportsCount}{" "}
+                  reports will be affected.
+                  <br /> <br />{" "}
+                  <b>
+                    Once you delete this dataset there is no turning back as
+                    this
+                    <br />
+                    action is irreversible.
+                  </b>
+                </p>
+                <div
+                  css={`
+                    margin-top: 32px;
+                  `}
+                >
+                  <input
+                    type="text"
+                    placeholder='Type "DELETE" to confirm'
+                    onChange={handleInputChange}
+                    onKeyPress={onInputEnter}
+                    css={`
+                      border: 1px solid #231d2c;
+                      border-radius: 10px;
+                      background: #ffffff;
+                      height: 48px;
+                      width: 400px;
+                      padding: 0px 24px;
+                      :focus,
+                      :active,
+                      :hover {
+                        outline: 1px solid #6061e5;
+                      }
+                    `}
+                    data-cy="delete-dataset-item-input"
+                  />
+                </div>
+              </div>
               <div
                 css={`
-                  margin-top: 3rem;
-                `}
-              >
-                <input
-                  type="text"
-                  placeholder='Type "DELETE" to confirm'
-                  onChange={handleInputChange}
-                  onKeyPress={onInputEnter}
-                  css={`
-                    border: 1px solid #231d2c;
-                    border-radius: 10px;
-                    background: #ffffff;
-                    height: 48px;
-                    width: 100%;
-                    padding: 0px 24px;
-                    :focus,
-                    :active,
-                    :hover {
-                      outline: 1px solid #6061e5;
-                    }
-                  `}
-                  data-cy="delete-dataset-item-input"
-                />
-              </div>
-            </div>
-            <div
-              css={`
-                display: flex;
-                justify-content: flex-end;
-                margin-top: 3rem;
-
-                margin-bottom: 2rem;
-                padding-right: 1rem;
-              `}
-            >
-              <button
-                type="submit"
-                disabled={!props.enableButton}
-                css={`
-                  background: ${props.enableButton ? "#231D2C" : "#e4e4e4"};
-                  border-radius: 30px;
-                  width: 107px;
-                  height: 41px;
-                  outline: none;
-                  border: none;
-                  text-transform: uppercase;
-                  color: #ffffff;
                   display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  cursor: pointer;
+                  justify-content: flex-end;
+                  margin-top: 36px;
                 `}
               >
-                Delete
-              </button>
-            </div>
-          </form>
-        </div>
+                <button
+                  type="submit"
+                  disabled={!props.enableButton}
+                  css={`
+                    background: ${props.enableButton ? "#231D2C" : "#e4e4e4"};
+                    border-radius: 30px;
+                    width: 107px;
+                    height: 41px;
+                    outline: none;
+                    border: none;
+                    text-transform: uppercase;
+                    color: #ffffff;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    cursor: pointer;
+                  `}
+                >
+                  Delete
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
       </Modal>
     </div>
   );
