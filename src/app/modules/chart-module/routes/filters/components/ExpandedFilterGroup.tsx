e02dc16ab6421d16a569923330b6ec154b3e5175
okorie2 <ellahokorie@gmail.com>
2024-05-01 11:12:54 +0100

    style: adjust font family

--- src/app/modules/chart-module/routes/filters/components/ExpandedFilterGroup.tsx
+++ src/app/modules/chart-module/routes/filters/components/ExpandedFilterGroup.tsx
@@ -273,7 +273,6 @@ export function ExpandedFilterGroup(props: ExpandedFilterGroupProps) {
               white-space: nowrap;
               text-overflow: ellipsis;
               font-size: 14px;
-              font-family: "GothamNarrow-Bold", "Helvetica Neue", sans-serif;
             `}
           >
             {splitStrBasedOnCapitalLetters(
