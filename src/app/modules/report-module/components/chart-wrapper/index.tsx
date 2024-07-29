1aa9ab79e3c2c7034f59a82456b28e0596d0eac7
okorie2 <ellahokorie@gmail.com>
2024-07-29 13:50:40 +0100

    feat:DX-1567; modify API hook- make token compulsory

--- src/app/modules/report-module/components/chart-wrapper/index.tsx
+++ src/app/modules/report-module/components/chart-wrapper/index.tsx
@@ -38,7 +38,10 @@ export function ReportChartWrapper(props: Props) {
     (actions) => actions.charts.ChartGetInReport.clear
   );
   const [datasetId, setDatasetId] = React.useState<string | null>(null);
-  const { datasetDetails } = useLoadDatasetDetails(datasetId!);
+  const { datasetDetails } = useLoadDatasetDetails(
+    datasetId!,
+    token ?? undefined
+  );
 
   const [_rawViz, setRawViz] = React.useState<any>(null);
   const {
@@ -51,7 +54,7 @@ export function ReportChartWrapper(props: Props) {
     setChartErrorMessage,
     setVisualOptions,
     setNotFound,
-  } = useRenderChartFromAPI(props.id);
+  } = useRenderChartFromAPI(props.id, token);
 
   const renderedChart = React.useMemo(() => {
     return chartFromAPI
