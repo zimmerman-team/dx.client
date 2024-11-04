a99835b8a2be0bc307e29b723b4f68bd298f62c6
okorie2 <ellahokorie@gmail.com>
2024-11-04 22:20:30 +0000

    feat: add tooltips to chart step buttons

--- src/app/modules/chart-module/components/toolbox/data.tsx
+++ src/app/modules/chart-module/components/toolbox/data.tsx
@@ -52,27 +52,43 @@ export const toolboxNavContent = (
   name: ToolboxNavType;
   icon: JSX.Element;
   path: string;
+  tooltip: string;
+  displayTooltip: boolean;
 }[] => [
   {
     name: "dataset",
     icon: <TableChartIcon />,
     path: `/chart/${page}/preview-data`,
+    tooltip: "Dataset Selection",
+    displayTooltip: false,
   },
   {
     name: "chart",
     icon: <AssessmentIcon />,
     path: `/chart/${page}/chart-type`,
+    tooltip: "Chart type",
+    displayTooltip: false,
   },
   {
     name: "mapping",
     icon: <CloudDoneIcon />,
     path: `/chart/${page}/mapping`,
+    tooltip: "Mapping",
+    displayTooltip: false,
   },
 
-  { name: "filters", icon: <TuneIcon />, path: `/chart/${page}/filters` },
+  {
+    name: "filters",
+    icon: <TuneIcon />,
+    path: `/chart/${page}/filters`,
+    tooltip: "Filter",
+    displayTooltip: false,
+  },
   {
     name: "customize",
     icon: <PaletteIcon />,
     path: `/chart/${page}/customize`,
+    tooltip: "Customisation",
+    displayTooltip: false,
   },
 ];
